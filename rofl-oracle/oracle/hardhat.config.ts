cdb6e6769ded986ba16781237c122255d59daa8b
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-09-21 17:03:55 +0530

    complete rofl app

---
+++ rofl-oracle/oracle/hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import "./src/tasks";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    sapphire: {
+      url: "https://sapphire.oasis.io",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 0x5afe,
+    },
+    "sapphire-testnet": {
+      url: "https://testnet.sapphire.oasis.io",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 0x5aff,
+    },
+    "sapphire-localnet": {
+      url: "http://localhost:8545",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 0x5afd,
+    },
+  },
+};
+
+export default config;
