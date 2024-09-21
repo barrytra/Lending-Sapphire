f4c0dd8b51d81d03061f44f01f61f30692f6fdfc
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-09-22 05:41:45 +0800

    pyth price feed

--- rofl-oracle/src/main.rs
+++ rofl-oracle/src/main.rs
@@ -60,14 +60,14 @@ impl OracleApp {
         let observation = tokio::task::spawn_blocking(move || -> Result<_> {
             // Request some data from Coingecko API.
             let rsp: serde_json::Value = rofl_utils::https::agent()
-                .get("https://www.binance.com/api/v3/ticker/price?symbol=ROSEUSDT")
+                .get("https://hermes.pyth.network/v2/updates/price/latest?ids%5B%5D=0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43&ids%5B%5D=0xc96458d393fe9deb7a7d63a0ac41e2898a67a7750dbd166673279e06c868df0a")
                 .call()?
                 .body_mut()
                 .read_json()?;
 
             // Extract price and convert to integer.
             let price = rsp
-                .pointer("/price")
+                .pointer("/parsed/0/price/price")
                 .ok_or(anyhow::anyhow!("price not available"))?
                 .as_str().unwrap()
                 .parse::<f64>()?;
